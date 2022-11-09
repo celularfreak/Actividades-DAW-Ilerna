c313381cda9f07e108aa480fa23c68d75057db13
celularfreak <celularfreak@hotmail.com>
2022-11-09 21:56:09 +0100

    primera subida

--- Desarrollo web en  entorno servidor/plantillaPAC/conexion.php
+++ Desarrollo web en  entorno servidor/plantillaPAC/conexion.php
@@ -1,19 +1,20 @@
 <?php 
 
 	function crearConexion() {
-		// Cambiar en el caso en que se monte la base de datos en otro lugar
 		$host = "localhost";
 		$user = "root";
 		$pass = "";
 		$baseDatos = "pac3_daw";
 
-		// Completar...
+		$conexion = mysqli_connect($host, $user, $pass, $baseDatos);
+
+		return $conexion;
 	}
 
 
 	function cerrarConexion($conexion) {
-		// Completar...
+		mysqli_close($conexion);
 	}
 
-
+	
 ?>
